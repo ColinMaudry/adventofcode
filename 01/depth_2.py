0c34a2817d9987fce366b1414e9ffd518c60c9ff
Colin Maudry <colin@maudry.com>
2021-12-01 11:24:03 +0100

    Day 1 removed try/except IndexError

--- 01/depth_2.py
+++ 01/depth_2.py
@@ -4,11 +4,7 @@ count = 0
 triple = []
 
 for i in range(len(depths)):
-    try:
-        triple = depths[i:i+3]
-    except IndexError:
-        print('Not enough values to make a triple')
-        exit(0)
+    triple = depths[i:i+3]
     triple_sum = sum(triple)
     if 0 < previous_triple_sum < triple_sum:
         count += 1
